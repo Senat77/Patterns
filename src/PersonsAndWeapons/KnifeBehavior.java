38f3024e114193cf051bfff81231cdf32d69890c
Sergii Senatorov <sergii.senatorov@gmail.com>
2019-04-03 15:48:10 +0300

    PersonsAndWeapons (not completed)

---
+++ src/PersonsAndWeapons/KnifeBehavior.java
@@ -0,0 +1,9 @@
+package PersonsAndWeapons;
+
+public class KnifeBehavior implements WeaponBehavior {
+
+    @Override
+    public void useWeapon() {
+        System.out.println("Stab!");
+    }
+}
---
+++ src/PersonsAndWeapons/WeaponBehavior.java
@@ -0,0 +1,6 @@
+package PersonsAndWeapons;
+
+public interface WeaponBehavior {
+
+    void useWeapon();
+}
