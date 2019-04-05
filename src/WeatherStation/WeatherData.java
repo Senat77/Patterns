c79464fa2844c776c8212ac6ff588cd15019c935
Sergii Senatorov <sergii.senatorov@gmail.com>
2019-04-05 12:43:19 +0300

    WeatherStation (not completed)

--- src/WeatherStation/WeatherData.java
+++ src/WeatherStation/WeatherData.java
@@ -1,4 +1,42 @@
 package WeatherStation;
 
-public class WeatherData {
+import java.util.ArrayList;
+
+public class WeatherData implements Subject {
+
+    private ArrayList observers;
+    private float temperature;
+    private float humidity;
+    private float pressure;
+
+    public WeatherData() {
+        observers = new ArrayList();
+    }
+
+    @Override
+    public void registerObserver(Observer o) {
+        observers.add(o);
+    }
+
+    @Override
+    public void removeObserver(Observer o) {
+        if(observers.indexOf(o) >= 0)
+            observers.remove(o);
+    }
+
+    @Override
+    public void notifyObservers() {
+        observers.forEach(o -> o.update(temperature, humidity, pressure));
+    }
+
+    public void measurementsChanged() {
+        notifyObservers();
+    }
+
+    public void setMeasurements(float temperature, float humidity, float pressure) {
+        this.temperature = temperature;
+        this.humidity = humidity;
+        this.pressure = pressure;
+        measurementsChanged();
+    }
 }
---
+++ src/WeatherStation/WeatherStation.java
@@ -0,0 +1,7 @@
+package WeatherStation;
+
+public class WeatherStation {
+    public static void main(String[] args) {
+
+    }
+}
