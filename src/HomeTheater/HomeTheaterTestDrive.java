4224237cc55c8acb8ee7af6834cc144df1c7e378
Sergii Senatorov <sergii.senatorov@gmail.com>
2019-07-22 15:21:03 +0300

    HomeTheaterFacade

--- src/HomeTheater/HomeTheaterTestDrive.java
+++ src/HomeTheater/HomeTheaterTestDrive.java
@@ -6,7 +6,8 @@ public class HomeTheaterTestDrive {
 
     public static void main(String[] args) {
         HomeTheaterFacade facade = new HomeTheaterFacade(new Amplifier(), new Tuner(), new DvdPlayer(), new CdPlayer(),
-                                                            new Projector(), new TheaterLights(), new Screen(), new PopcornPopper());
+                                                            new Projector(), new TheaterLights(), new Screen(),
+                                                            new PopcornPopper());
 
         facade.watchMovie("Test movie");
         System.out.println("==========");
